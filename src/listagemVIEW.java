40711cffaa014471e5a15e221e50dfdf9cbcd46f
Elane2020 <elanedosanjos27@gmail.com>
2026-09-15 14:54:03 -0300

    criada a tela de vendas

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -187,9 +187,9 @@ public class listagemVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
-   //     vendasVIEW vendas = new vendasVIEW(); 
-   //     vendas.setVisible(true);
-  //      vendas.requestFocus();
+        vendasVIEW vendas = new vendasVIEW(); 
+        vendas.setVisible(true);
+        vendas.requestFocus();
        
     }//GEN-LAST:event_btnVendasActionPerformed
 
